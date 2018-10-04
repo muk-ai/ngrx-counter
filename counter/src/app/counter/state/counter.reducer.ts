869603968f724f903498b2640a9ac16ea9b4a117
Kenta Mukai <r@muk.ai>
2018-10-04 16:22:15 +0900

    :sparkles: define reducer

--- counter/src/app/counter/state/counter.reducer.ts
+++ counter/src/app/counter/state/counter.reducer.ts
@@ -1,18 +1,25 @@
 import { Action } from '@ngrx/store';
-
+import { CounterActionTypes } from './counter.actions';
 
 export interface State {
-
+  count: number;
 }
 
 export const initialState: State = {
-
+  count: 0
 };
 
 export function reducer(state = initialState, action: Action): State {
   switch (action.type) {
-
+    case CounterActionTypes.CountIncrement:
+      // state.count++; return state; とはせずに新しくObjectを返す
+      return {...state, count: state.count + 1};
+    case CounterActionTypes.CountDecrement:
+      // state.count--; return state; とはせずに新しくObjectを返す
+      return {...state, count: state.count - 1};
     default:
       return state;
   }
 }
+
+export const getCount = (state: State) => state.count;
