e528c2e674bb3e148f7f278c167fbccdca6786ec
Kenta Mukai <r@muk.ai>
2018-10-04 15:34:58 +0900

    :sparkles: npx ng g component counter --module=counter/counter.module.ts --export

--- counter/src/app/counter/counter.module.ts
+++ counter/src/app/counter/counter.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterComponent } from './counter.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CounterComponent],
+  exports: [CounterComponent]
 })
 export class CounterModule { }
