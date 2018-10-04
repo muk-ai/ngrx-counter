5492d8e7a4b286b918aa4a58d8196941b60058cf
Kenta Mukai <r@muk.ai>
2018-10-04 16:00:24 +0900

    :sparkles: create action for counter
    npx ng g action counter/state/counter --flat

---
+++ counter/src/app/counter/state/counter.actions.ts
@@ -0,0 +1,11 @@
+import { Action } from '@ngrx/store';
+
+export enum CounterActionTypes {
+  LoadCounters = '[Counter] Load Counters'
+}
+
+export class LoadCounters implements Action {
+  readonly type = CounterActionTypes.LoadCounters;
+}
+
+export type CounterActions = LoadCounters;
