a86c61ec24b919c840591bc358a1dc4cad9eda6b
Kenta Mukai <r@muk.ai>
2018-10-05 14:36:49 +0900

    :sparkles: add it for debug

--- counter/src/app/counter/state/counter.effects.ts
+++ counter/src/app/counter/state/counter.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { delay, mergeMap } from 'rxjs/operators';
+import { delay, mergeMap, tap } from 'rxjs/operators';
 
 import { CounterActionTypes, CountDelayedDouble, CountDouble } from './counter.actions';
 import { from, of } from 'rxjs';
@@ -14,6 +14,15 @@ export class CounterEffects {
     ofType<CountDelayedDouble>(CounterActionTypes.CountDelayedDouble),
     mergeMap(action => {
       return from(of(new CountDouble()).pipe(delay(action.payload.delay)));
+    }),
+  );
+
+  @Effect({
+    dispatch: false
+  })
+  logActions$ = this.actions$.pipe(
+    tap(action => {
+      console.log(action);
     }),
   );
 
